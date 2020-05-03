47410a421754a30effe22e1566b262c2bb486fa2
lamboktulus1379 <lamboktulus1379@gmail.com>
2020-05-03 19:38:22 +0700

    Added Product Categories Controller

---
+++ yd/Controllers/ProductCategories.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using yd.Models;
+
+namespace yd.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductCategories : ControllerBase
+    {
+        private readonly YDContext _context;
+
+        public ProductCategories(YDContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ProductCategory>>> GetProductCategories()
+        {
+            return await _context.ProductCategories.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<ProductCategory>> GetProductCategory(int id)
+        {
+            var productCategory = await _context.ProductCategories.FindAsync(id);
+
+            if (productCategory == null)
+            {
+                return NotFound();
+            }
+            return productCategory;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<ProductCategory>> PostProductCategory(ProductCategory productCategory)
+        {
+            _context.ProductCategories.Add(productCategory);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetProductCategory", new { id = productCategory.Id }, productCategory);
+
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutProductCategory(int id, ProductCategory productCategory)
+        {
+            if (id != productCategory.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(productCategory).State = EntityState.Modified;
+
+            try
+            {
+              await  _context.SaveChangesAsync();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            return Ok(productCategory);
+        }
+
+    }
+}
